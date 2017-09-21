1ecc84eb75d68d46dd9217fba52bf595e3eb86af
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-09-21 18:09:39 +0200

    class Participant created to handle the datas about the participants then code was refactored to reflect that change

--- secretsanta.py
+++ secretsanta.py
@@ -1,24 +1,51 @@
 import random
+import itertools
 import datas
+
+class Participant():
+    newID = itertools.count().__next__
+
+    def __init__(self, name):
+        self.name = name
+        self.id = Participant.newID()
+        self.email = ''
+        self.giftee = ''
+        self.gifter = ''
+        self.blacklist = []
+
+    def __repr__(self):
+        return self.name
+
+    def getName(self):
+        return self.name
+
+    def getGiftee(self):
+        return self.giftee
+
+    def getGifter(self):
+        return self.gifter
+
+    def getBlacklist(self):
+        return self.blacklist
 
 def valid_pick(pick, constraints):
     '''
     Arguments :
-        - pick, a list of participants (str)
-        - constraints, a dictionary with participants (str) as indexes and tuples of participants (str) as values
+        - pick, a list of Participant objects
+        - constraints, a dictionary with participants' names (str) as indexes and tuples of participants (str) as values
     Returns :
         - True if the pick is abiding to the constraints, False otherwise
     '''
     for idx, participant in enumerate(pick[:-1]):
-        if pick[idx+1] in constraints[participant]:
+        if pick[idx+1].getName() in constraints[participant.getName()]:
             return False
-    return pick[-1] not in constraints[pick[0]] and pick[0] not in constraints[pick[-1]]
+    return pick[-1].getName() not in constraints[pick[0].getName()] and pick[0].getName() not in constraints[pick[-1].getName()]
 
 def matching_participants(participants, constraints):
     '''
     Arguments :
-        - participants (list) of names (str) forming an hamiltonian path
-        - constraints, a dictionary with participants (str) as indexes and tuples of participants (str) as values
+        - participants (list) of Participant objects
+        - constraints, a dictionary with participants' names (str) as indexes and tuples of participants (str) as values
     Returns :
         - pick, same format as participants but shuffled and abiding to the constaints
     '''
@@ -31,7 +58,7 @@ def matching_participants(participants, constraints):
 def create_pairs(valid_pick):
     '''
     Arguments :
-        - valid_pick, list of participants (str) forming a correct hamiltonian path
+        - valid_pick, list of Participant objects forming a correct hamiltonian path
     Returns :
         - couples : list of tuples each containing the pair (gifter, giftee)
     '''
@@ -41,6 +68,7 @@ def create_pairs(valid_pick):
     return couples
 
 if __name__ == '__main__':
-    pick = matching_participants(datas.participants, datas.constraints)
+    participants = [Participant(name) for name in datas.participants]
+    pick = matching_participants(participants, datas.constraints)
     couples = create_pairs(pick)
     print(couples)
