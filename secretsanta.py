159c63b825e5a5a6da111582df0e91f44700c695
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-09-21 08:23:34 +0200

    docstrings added

--- secretsanta.py
+++ secretsanta.py
@@ -2,12 +2,26 @@ import random
 import datas
 
 def valid_pick(pick, constraints):
+    '''
+    Arguments :
+        - pick, a list of participants (str)
+        - constraints, a dictionary with participants (str) as indexes and tuples of participants (str) as values
+    Returns :
+        - True if the pick is abiding to the constraints, False otherwise
+    '''
     for idx, participant in enumerate(pick[:-1]):
         if pick[idx+1] in constraints[participant]:
             return False
     return pick[-1] not in constraints[pick[0]] and pick[0] not in constraints[pick[-1]]
 
 def matching_participants(participants, constraints):
+    '''
+    Arguments :
+        - participants (list) of names (str)
+        - constraints, a dictionary with participants (str) as indexes and tuples of participants (str) as values
+    Returns :
+        - pick, same format as participants but shuffled and abiding to the constaints
+    '''
     while True:
         pick = list(participants)
         random.shuffle(pick)
